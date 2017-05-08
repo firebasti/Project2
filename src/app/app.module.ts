1cd000783a01bb2875d1cef100fe34300581ab73
Sebastian Walter <sebastian.walter1@allianz.de>
2017-05-08 14:21:43 +0200

    brings hoch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponentComponent } from './hero-detail-component/hero-detail-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponentComponent
   ],
   imports: [
     BrowserModule,
