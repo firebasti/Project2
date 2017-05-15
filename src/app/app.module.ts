322f6898f5854a4419c5c2275ec3f42dc20509c7
Sebastian Walter <sebastian.walter1@allianz.de>
2017-05-15 09:34:07 +0200

    skldfj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponentComponent } from './hero-detail-component/hero-detail-component.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
